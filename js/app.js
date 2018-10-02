a92a1d86dde8fe996e4b11074f96b8c84011ac24
Luan Portela Barbosa <luanpb@live.com>
2018-10-02 17:44:01 -0300

    removendo li

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 /*
  * Create a list that holds all of your cards
  */
-let cards = []
+let cards = [],
+  icones = [
+    'fa fa-diamond',
+    'fa fa-paper-plane-o',
+    'fa fa-anchor',
+    'fa fa-bolt',
+    'fa fa-cube',
+    'fa fa-leaf',
+    'fa fa-bicycle',
+    'fa fa-bomb'
+  ];
 
 let criarCards = (arr) => {
   for (let i = 0; i < 16; i++){
